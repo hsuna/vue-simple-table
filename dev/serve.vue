1e8e803346f04154ac39fe59d1a05bac0e9fb482
hsuna <me@hsuna.com>
2021-04-19 23:03:42 +0800

    feat: init

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import SimpleTable from '@/simple-table.vue';
+
+export default defineComponent({
+  name: 'ServeDev',
+  components: {
+    SimpleTable
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <simple-table />
+  </div>
+</template>
